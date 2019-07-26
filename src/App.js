448928b899fd4be510bd004b951a163b13b6134f
Phone Nyo <phonenai2014@gmail.com>
2019-07-26 18:34:32 +0630

    state & data passing to child component & props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
-// import React, { Component } from 'react';
+// import React from 'react';
+import React, { Component } from 'react';
 import Todos from './components/Todos';
 import './App.css';
 
-// class App extends Component {
-//   render() {
-function App() {
-  return (
-    <div className="App">
-        <Todos />
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+          {
+            id: 1,
+            title: 'Having breakfast',
+            completed: false
+          },
+          {
+            id: 2,
+            title: 'Coding smart',
+            completed: false
+          },
+          {
+            id: 3,
+            title: 'Meeting with Client',
+            completed: false
+          }
+    ]
+  };
+  render() {
+// function App() {
+    // console.log(this.state.todos)
+    return (
+      <div className="App">
+          <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
-// }
 export default App;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 
 class Todos extends Component {
   render() {
-// function Todos() {
-	  return (
-	        <h1>Hello React</h1>
-	  );
+	  return this.props.todos.map((todo) => (		//return ဆိုရင် {} မလို
+	  	<h1>{todo.title}</h1>
+	  ));
 	}
 }
 export default Todos;
