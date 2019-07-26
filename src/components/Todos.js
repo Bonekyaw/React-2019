4e0e8aa5869e6b2059044ffcb35da99e2be3a8c1
Phone Nyo <phonenai2014@gmail.com>
2019-07-26 16:19:34 +0630

    Created component

---
+++ src/components/Todos.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+  render() {
+// function Todos() {
+	  return (
+	        <h1>Hello React</h1>
+	  );
+	}
+}
+export default Todos;
