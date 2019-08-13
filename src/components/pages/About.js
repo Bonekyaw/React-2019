ee44a4458e0f24a0f3bc76a488f79404480f9d8e
Phone Nyo <phonenai2014@gmail.com>
2019-08-13 14:58:31 +0630

    React Router

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About() {
+	return (
+		<React.Fragment>
+			<h1>About</h1>
+			<p>This is the about page</p>
+		</React.Fragment>
+	)
+}
+
+// React.Fragment is used instead of <div>
+// Shorter Syntax is <> </>
